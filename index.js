6488e780190a8f378fdc150550046a85adcaffa0
Harsh Sharma <Harshsharma2345@gmail.com>
2020-12-14 14:05:53 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,11 @@
+function pausevideo(){
+vid=document.getElementById('myVideo');
+vid.pause();
+console.log('i executed');
+}
+
+function play(){
+    video=document.getElementById('myVideo');
+    video.play();
+    console.log('i executed');
+}
