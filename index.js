393a3335d31a98c5598c5d7951d54faca2f212a9
Harsh Sharma <Harshsharma2345@gmail.com>
2020-12-23 22:06:48 +0530

    first commit

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function play(){
     video.play();
     console.log('i executed');
 }
+
+
+
+  var i = 0;
+  var speed = 50; /* The speed/duration of the effect in milliseconds */
+  
+  function typeWriter() {
+    if (i < 10) {
+      document.getElementById("stats").innerHTML += 1;
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
